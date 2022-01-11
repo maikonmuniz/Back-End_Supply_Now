be840b5385ce4dd72e4a3aad4671ca60dbe95757
maikonmuniz <maikon.muniz@yahoo.com.br>
2022-01-11 01:14:53 -0300

    configuração do mongo db atlas corregindo erros

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const DB_USER = process.env.DB_USER
+const DB_USER = process.env.DB_USER  
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
-let db = 'mongodb://localhost:27017/supply_now'
 
-if(process.env.NODE_ENV == "production"){
-    db = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rn0e1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+async function main(){
+    
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rn0e1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+
 }
 
-async function main() {
-    await mongoose.connect(db)
-    console.log("Conectou ao mongoose!")
-}
+main().then(() => {
+    console.log("Conectou")
+}).catch((err) => console.log(err))
 
-main().catch((err) => console.log(err))
-
-module.exports = mongoose
+module.exports = mongoose   
