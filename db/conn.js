0482f8057d38169cb335c826459bc85473dd592b
maikonmuniz <maikon.muniz@yahoo.com.br>
2022-01-10 23:32:47 -0300

    mongodb Atlas configuração para deploy

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+let db = 'mongodb://localhost:27017/supply_now'
+
+if(process.env.NODE_ENV == "production"){
+    db = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rn0e1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/supply_now')
+    await mongoose.connect(db)
     console.log("Conectou ao mongoose!")
 }
 
