b4e10c1a3c067f93e47723ce546a45eb7211733b
JoaoAlves92 <jvamarangonia@gmail.com>
2022-01-12 01:05:09 -0300

    adjusting db conn

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 require('dotenv').config()
 const DB_USER = process.env.DB_USER  
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 
-async function main(){
-    
-    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rn0e1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.rn0e1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
-}
+mongoose.connect(uri, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    }).then(() => {
+        console.log('Conectado com sucesso')
+    }).catch((error) => {
+        console.log(error)
+    })
 
-main().then(() => {
-    console.log("Conectou")
-}).catch((err) => console.log(err))
+mongoose.Promise = global.Promise
 
-module.exports = mongoose   
+module.exports = mongoose;
